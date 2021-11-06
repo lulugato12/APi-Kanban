3e0aca2ec657a1addc56b247c28438ff07d18c12
Lourdes Cajica <A01732177@itesm.mx>
2021-11-05 20:06:55 -0600

    fix the columns

--- models/role.js
+++ models/role.js
@@ -3,14 +3,16 @@ const {conect_db, close_db} = require('../helpers/server')
 
 const sequelize = conect_db();
 
-const Role = sequelize.define('Role', {
+const Role = sequelize.define('role', {
+  role_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: false
   }
-}, {});
-
-sequelize.close_db();
+}, {freezeTableName: true});
 
 module.exports = {
   Role
