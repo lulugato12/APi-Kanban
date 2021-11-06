5cf6a35f14b4c67f0829f128a891d24c35dd6803
Lourdes Cajica <A01732177@itesm.mx>
2021-11-06 16:31:46 -0600

    add the station model

---
+++ models/station.js
@@ -0,0 +1,47 @@
+const {DataTypes, Deferrable} = require('sequelize');
+const {conect_db, close_db} = require('../helpers/server');
+const {Drawer} = require('./drawer');
+const {User} = require('./user');
+
+const sequelize = conect_db();
+
+const Station = sequelize.define('station', {
+  station_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'user_id',
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+    }
+  },
+  input_drawer_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Drawer,
+      key: 'drawer_id',
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+    }
+  },
+  output_drawer_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Drawer,
+      key: 'drawer_id',
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+    }
+  }
+}, {freezeTableName: true});
+
+module.exports = {
+  Station
+}
