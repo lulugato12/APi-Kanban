e56c32c51d17c67104170d58deef1ae47cc9b4f3
Lourdes Cajica <A01732177@itesm.mx>
2021-11-06 14:39:31 -0600

    add the user model

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const {DataTypes, Deferrable} = require('sequelize');
+const {conect_db, close_db} = require('../helpers/server')
+const {Role} = require('./role')
+
+const sequelize = conect_db();
+
+const User = sequelize.define('user_kanban', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  first_name: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  last_name: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  id_telegram: {
+    type: DataTypes.STRING(50)
+  },
+  major: {
+    type: DataTypes.STRING(50)
+  },
+  class: {
+    type: DataTypes.STRING(50),
+  },
+  email: {
+    type: DataTypes.STRING(20)
+  },
+  role_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Role,
+      key: 'role_id',
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+    }
+  }
+}, {freezeTableName: true});
+
+module.exports = {
+  User
+}
