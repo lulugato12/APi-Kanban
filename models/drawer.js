813fa89d440b6b928b855584ef2ed3605cfe85de
Lourdes Cajica <A01732177@itesm.mx>
2021-11-06 16:16:51 -0600

    add the drawer model

---
+++ models/drawer.js
@@ -0,0 +1,39 @@
+const {DataTypes, Deferrable} = require('sequelize');
+const {conect_db, close_db} = require('../helpers/server')
+const {Product} = require('./product')
+const {Kanban} = require('./kanban');
+
+const sequelize = conect_db();
+
+const Drawer = sequelize.define('drawer', {
+  drawer_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  capacity: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Product,
+      key: 'product_id',
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+    }
+  },
+  kanban_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Kanban,
+      key: 'kanban_id',
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+    }
+  }
+}, {freezeTableName: true});
+
+module.exports = {
+  Drawer
+}
