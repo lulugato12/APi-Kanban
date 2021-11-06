8482a85bdd9a38b25991294ae938a93ce59dbae2
Lourdes Cajica <A01732177@itesm.mx>
2021-11-06 16:09:58 -0600

    add the layout model

---
+++ models/layout.js
@@ -0,0 +1,29 @@
+const {DataTypes, Deferrable} = require('sequelize');
+const {conect_db, close_db} = require('../helpers/server')
+const {User} = require('./user')
+
+const sequelize = conect_db();
+
+const Layout = sequelize.define('layout', {
+  layout_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING(80),
+    allowNull: false
+  },
+  user_id:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'user_id',
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+    }
+  }
+}, {freezeTableName: true});
+
+module.exports = {
+  Layout
+}
