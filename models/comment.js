b47f57a5ca39e5c3b6507187e9f50668fe93cc83
Lourdes Cajica <A01732177@itesm.mx>
2021-11-06 14:59:33 -0600

    add the comment model

---
+++ models/comment.js
@@ -0,0 +1,29 @@
+const {DataTypes, Deferrable} = require('sequelize');
+const {conect_db, close_db} = require('../helpers/server')
+const {User} = require('./user')
+
+const sequelize = conect_db();
+
+const Comment = sequelize.define('comment', {
+  comment_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  text: {
+    type: DataTypes.STRING(500),
+    allowNull: false
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'user_id',
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+    }
+  }
+}, {freezeTableName: true});
+
+module.exports = {
+  Comment
+}
