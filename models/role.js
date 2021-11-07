f727c4616dd787a55056d644d52af6316cc1a5c0
Lourdes Cajica <A01732177@itesm.mx>
2021-11-06 20:02:44 -0600

    add comments

--- models/role.js
+++ models/role.js
@@ -1,8 +1,16 @@
+/**
+ * @module role
+ * PostgreSQL table: role
+ */
+
 const {DataTypes} = require('sequelize');
-const {conect_db, close_db} = require('../helpers/server')
+const {conect_db, close_db} = require('../helpers/server');
 
 const sequelize = conect_db();
 
+/**
+ * Definition of the parameters of the role model.
+ */
 const Role = sequelize.define('role', {
   role_id: {
     type: DataTypes.INTEGER,
