632cb3786ec5843158cabb6ca8a973cb6f11151c
jaorsa <38358814+jaorsa@users.noreply.github.com>
2021-11-28 15:27:18 -0600

    Changed database credentials

--- helpers/server.js
+++ helpers/server.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 const user = 'postgres';
-const pass = 'Gatofeliz18';
+const pass = 'B4k4.4B6579{|}';
 const host = 'localhost';
 const port = '5432';
-const db = 'kanban';
+const db = 'kanban_digital';
 
 function conect_db(){
   const sequelize = new Sequelize(db, user, pass, {
